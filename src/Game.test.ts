5f7f43200dc14a8dd97e1e442a3584ecb72fa176
Panagiotis Smyrnis <panagiotis.smyrnis@cazoo.co.uk>
2021-07-02 12:10:45 +0100

    Refactoring

--- src/Game.test.ts
+++ src/Game.test.ts
@@ -1,5 +1,5 @@
 
-import { Game } from "./Game"
+import { Game, Coordinates, Player } from "./Game"
 
 describe('TicTacToe game', () => {
     let game : Game;
@@ -8,98 +8,98 @@ describe('TicTacToe game', () => {
         game = new Game();
     });
 
-    it('should not allow player O to play first', () => {
-        expect(() => game.Play('O', 0, 0)).toThrow();
+    // it('should not allow player O to play first', () => {
+    //     expect(() => game.Play(Player.playerTwo, { X:0, Y:0 } as Coordinates)).toThrow();
+    // });
+
+    // it('should not allow player x to play twice in a row', () =>{
+    //     game.Play(Player.playerOne, { X:0, Y:0 } as Coordinates);
+    //     expect(() => game.Play(Player.playerOne, { X:1, Y:0 } as Coordinates)).toThrow();
+    // });
+
+    it('should not allow a player to play in last played position', () => {
+        game.Play({ X:0, Y:0 } as Coordinates);
+        expect(() => game.Play({ X:0, Y:0 } as Coordinates)).toThrow();
     });
 
-    it('should not allow player x to play twice in a row', () =>{
-        game.Play('X', 0, 0);
-        expect(() => game.Play('X', 1, 0)).toThrow();
-    });
-    
-    it('should not allow a player to play in last played position', () => {
-        game.Play('X', 0, 0);
-        expect(() => game.Play('O', 0, 0)).toThrow();
-    });
-    
     it('should not allow a player to play in any played position', () => {
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
-        expect(() => game.Play('X', 0, 0)).toThrow();
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:0 } as Coordinates);
+        expect(() => game.Play({ X:0, Y:0 } as Coordinates)).toThrow();
     });
-    
+
     it('should declare player X as winner if it plays three in top row', () =>{
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
-        game.Play('X', 0, 1);
-        game.Play('O', 1, 1);
-        game.Play('X', 0, 2);
-        
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:0 } as Coordinates);
+        game.Play({ X:0, Y:1 } as Coordinates);
+        game.Play({ X:1, Y:1 } as Coordinates);
+        game.Play({ X:0, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("X");
     });
-    
+
     it('should declare player O as winner if it plays three in top row', () => {
-        game.Play('X', 1, 0);
-        game.Play('O', 0, 0);
-        game.Play('X', 1, 1);
-        game.Play('O', 0, 1);
-        game.Play('X', 2, 2);
-        game.Play('O', 0, 2);
-        
+        game.Play({ X:1, Y:0 } as Coordinates);
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:1 } as Coordinates);
+        game.Play({ X:0, Y:1 } as Coordinates);
+        game.Play({ X:2, Y:2 } as Coordinates);
+        game.Play({ X:0, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("O");
     });
-    
+
     it('should declare player X as winner if it plays three in middle row', () => {
-        game.Play('X', 1, 0);
-        game.Play('O', 0, 0);
-        game.Play('X', 1, 1);
-        game.Play('O', 0, 1);
-        game.Play('X', 1, 2);
-       
+        game.Play({ X:1, Y:0 } as Coordinates);
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:1 } as Coordinates);
+        game.Play({ X:0, Y:1 } as Coordinates);
+        game.Play({ X:1, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("X");
     });
-    
+
     it('should declare player O as winner if it plays three in middle row', () => {
-        game.Play('X', 0, 0);
-        game.Play('O', 1, 0);
-        game.Play('X', 2, 1);
-        game.Play('O', 1, 1);
-        game.Play('X', 2, 2);
-        game.Play('O', 1, 2);
-        
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:0 } as Coordinates);
+        game.Play({ X:2, Y:1 } as Coordinates);
+        game.Play({ X:1, Y:1 } as Coordinates);
+        game.Play({ X:2, Y:2 } as Coordinates);
+        game.Play({ X:1, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("O");
     });
-    
+
     it('should declare player X as winner if it plays three in bottom row', () => {
-        game.Play('X', 2, 0);
-        game.Play('O', 0, 0);
-        game.Play('X', 2, 1);
-        game.Play('O', 0, 1);
-        game.Play('X', 2, 2);
-        
+        game.Play({ X:2, Y:0 } as Coordinates);
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:2, Y:1 } as Coordinates);
+        game.Play({ X:0, Y:1 } as Coordinates);
+        game.Play({ X:2, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("X");
     });
-    
+
     it('should declare player O as winner if it plays three in bottom row', () => {
-        game.Play('X', 0, 0);
-        game.Play('O', 2, 0);
-        game.Play('X', 1, 1);
-        game.Play('O', 2, 1);
-        game.Play('X', 0, 1);
-        game.Play('O', 2, 2);
-        
+        game.Play({ X:0, Y:0 } as Coordinates);
+        game.Play({ X:2, Y:0 } as Coordinates);
+        game.Play({ X:1, Y:1 } as Coordinates);
+        game.Play({ X:2, Y:1 } as Coordinates);
+        game.Play({ X:0, Y:1 } as Coordinates);
+        game.Play({ X:2, Y:2 } as Coordinates);
+
         var winner = game.Winner();
-        
+
         expect(winner).toBe("O");
     });
 });
--- src/Game.ts
+++ src/Game.ts
@@ -1,103 +1,99 @@
-export class Game {
-    private _lastSymbol: string = ' ';
-    private _board: Board = new Board();
-
-    public Play(symbol: string, x: number, y: number) : void {
-        //if first move
-        if (this._lastSymbol == ' ') {
-            //if player is X
-            if (symbol == 'O') {
-                throw new Error("Invalid first player");
-            }
-        }
-        //if not first move but player repeated
-        else if (symbol == this._lastSymbol) {
-            throw new Error("Invalid next player");
-        }
-        //if not first move but play on an already played tile
-        else if (this._board.TileAt(x, y).Symbol != ' ') {
-            throw new Error("Invalid position");
-        }
-
-        // update game state
-        this._lastSymbol = symbol;
-        this._board.AddTileAt(symbol, x, y);
-    }
-
-    public Winner() : string {
-        //if the positions in first row are taken
-        if (this._board.TileAt(0, 0)!.Symbol != ' ' &&
-                this._board.TileAt(0, 1)!.Symbol != ' ' &&
-                this._board.TileAt(0, 2)!.Symbol != ' ') {
-            //if first row is full with same symbol
-            if (this._board.TileAt(0, 0)!.Symbol ==
-                    this._board.TileAt(0, 1)!.Symbol &&
-                    this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol) {
-                return this._board.TileAt(0, 0)!.Symbol;
-            }
-        }
-
-        //if the positions in first row are taken
-        if (this._board.TileAt(1, 0)!.Symbol != ' ' &&
-                this._board.TileAt(1, 1)!.Symbol != ' ' &&
-                this._board.TileAt(1, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(1, 0)!.Symbol ==
-                    this._board.TileAt(1, 1)!.Symbol &&
-                    this._board.TileAt(1, 2)!.Symbol ==
-                            this._board.TileAt(1, 1)!.Symbol) {
-                return this._board.TileAt(1, 0)!.Symbol;
-            }
-        }
-
-        //if the positions in first row are taken
-        if (this._board.TileAt(2, 0)!.Symbol != ' ' &&
-                this._board.TileAt(2, 1)!.Symbol != ' ' &&
-                this._board.TileAt(2, 2)!.Symbol != ' ') {
-            //if middle row is full with same symbol
-            if (this._board.TileAt(2, 0)!.Symbol ==
-                    this._board.TileAt(2, 1)!.Symbol &&
-                    this._board.TileAt(2, 2)!.Symbol ==
-                            this._board.TileAt(2, 1)!.Symbol) {
-                return this._board.TileAt(2, 0)!.Symbol;
-            }
-        }
-
-        return ' ';
-    }
-}
-
-interface Tile
-{
+export interface Coordinates {
     X: number;
     Y: number;
-    Symbol: string;
 }
 
-class Board
-{
-    private _plays : Tile[] = [];
+export enum Player {
+    playerOne = 'X',
+    playerTwo = 'O',
+}
 
-    constructor()
-    {
-        for (let i = 0; i < 3; i++)
-        {
-            for (let j = 0; j < 3; j++)
-            {
-                const tile : Tile = {X :i, Y:j, Symbol:" "};
+export class Game {
+    private _lastPlayer?: Player = undefined;
+    private _board: Board = new Board();
+
+    public Play(coordinates: Coordinates): void {
+        const player = this.setCurrentPlayer();
+        this.guardAgainstPlayingOnAnOccupiedPosition(coordinates);
+        this.setLastPlayer(player);
+        this.addTileToBoard(player, coordinates);
+    }
+
+    public Winner(): Player | undefined {
+        return this.getRowWinner(1) || this.getRowWinner(2) || this.getRowWinner(3);
+    }
+
+    private getRowWinner(rowNumber: number): Player | undefined {
+        if (this._board.TileAt({X: rowNumber - 1, Y: 0}).Symbol
+            && this._board.TileAt({X: rowNumber - 1, Y: 0}).Symbol === this._board.TileAt({X: rowNumber - 1, Y: 1}).Symbol
+            && this._board.TileAt({X: rowNumber - 1, Y: 2}).Symbol === this._board.TileAt({X: rowNumber - 1, Y: 1}).Symbol) {
+            return this._board.TileAt({X: rowNumber - 1, Y: 0}).Symbol;
+        }
+
+        return undefined;
+    }
+
+    private guardAgainstPlayingOnAnOccupiedPosition(coordinates : Coordinates):void {
+        if (this._board.TileAt(coordinates).Symbol) {
+              throw new Error("Invalid position");
+        }
+    }
+
+    private isFirstMove(): boolean {
+        return !this._lastPlayer;
+    }
+
+    private isFirstPlayer(player: Player): boolean {
+        return player === Player.playerOne;
+    }
+
+    private setLastPlayer(player: Player): void {
+        this._lastPlayer = player;
+    }
+
+    private getLastPlayer(): Player | undefined {
+       return this._lastPlayer;
+    }
+
+    private addTileToBoard(player: Player, coordinates: Coordinates): void {
+        this._board.AddTileAt(player, coordinates);
+    }
+
+    private setCurrentPlayer(){
+        if(this.isFirstMove()){
+            return Player.playerOne;
+        }
+
+        if(this.getLastPlayer() === Player.playerOne){
+            return Player.playerTwo;
+        }
+
+        return Player.playerOne;
+    }
+}
+
+interface Tile {
+    coordinates: Coordinates;
+    Symbol: Player | undefined;
+}
+
+class Board {
+    private _plays: Tile[] = [];
+
+    constructor() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                const tile: Tile = {coordinates: {X: i, Y: j}, Symbol: undefined};
                 this._plays.push(tile);
             }
         }
     }
 
-    public TileAt(x:  number, y: number): Tile {
-        return this._plays.find((t:Tile) => t.X == x && t.Y == y)!
+    public TileAt(coordinates: Coordinates): Tile {
+        return this._plays.find((t: Tile) => t.coordinates.X === coordinates.X && t.coordinates.Y === coordinates.Y)!
     }
 
-    public AddTileAt(symbol: string, x: number, y: number) : void
-    {
-        const tile : Tile = {X :x, Y:y, Symbol:symbol};
-
-        this._plays.find((t:Tile) => t.X == x && t.Y == y)!.Symbol = symbol;
+    public AddTileAt(player: Player, coordinates: Coordinates): void {
+        this._plays.find((t: Tile) => t.coordinates.X === coordinates.X && t.coordinates.Y === coordinates.Y)!.Symbol = player;
     }
 }
